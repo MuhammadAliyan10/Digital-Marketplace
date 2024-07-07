38bc3b8dd0ac44be352f8d6bce892d6d58046c88
Muhammad Aliyan <135156764+MuhammadAliyan10@users.noreply.github.com>
2024-07-07 17:06:00 +0500

    Add Admin Dashboard

---
+++ marketplace/src/getPayload.ts
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+import path from "path";
+import type { InitOptions } from "payload/config";
+import payload from "payload";
+
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
+let cached = (global as any).payload;
+if (!cached) {
+  cached = (global as any).payload = {
+    client: null,
+    promise: null,
+  };
+}
+interface Args {
+  initOption?: Partial<InitOptions>;
+}
+export const getPayload = async ({ initOption }: Args = {}) => {
+  if (!process.env.PAYLOAD_SECRET) {
+    throw new Error("Payload Secret not found");
+  }
+  if (cached.client) {
+    return cached.client;
+  }
+  if (!cached.client) {
+    cached.promise = payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOption?.express ? false : true,
+      ...(initOption || {}),
+    });
+  }
+  try {
+    cached.client = await cached.promise;
+  } catch (error: unknown) {
+    cached.promise = null;
+    throw error;
+  }
+  return cached.client;
+};
