93746113dad9eabee3d87d4e089280b64c3720b5
Muhammad Aliyan <135156764+MuhammadAliyan10@users.noreply.github.com>
2024-07-18 22:59:56 +0500

    Add Model & have type error

--- marketplace/src/payload.config.ts
+++ marketplace/src/payload.config.ts
@@ -5,12 +5,16 @@ import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
 import Users from "./collections/Users";
 import dotenv from "dotenv";
+import { Products } from "./collections/Products/Product";
+import { Media } from "./collections/Media";
+import { Orders } from "./collections/Orders";
+import { ProductFiles } from "./collections/ProductFile";
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
 });
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users],
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: "/sell",
   },
